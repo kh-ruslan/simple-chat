2eb8f4da2fd5412641add32b5a71a04c5b97147f
purple_swag <h.ruslan90@mail.ru>
2016-09-25 17:17:23 +0500

    init commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', (socket) => {
+	console.log('a user connected')
+	socket.on('disconnect', () => console.log('user disconnected')
+  )
+})
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg)
+  })
+})
+
+http.listen(3000, () => console.log('listening on *:3000'))
+
