014c35e65b34ff31c83720bcf9b2c28c60ac649a
purple_swag <h.ruslan90@mail.ru>
2016-09-29 20:47:52 +0500

    updated

--- app.js
+++ app.js
@@ -2,20 +2,30 @@ const express = require('express')
 const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
+var online_list = []
 
-app.use(express.static(__dirname + '/public'));
-
-io.on('connection', (socket) => {
-	console.log('a user connected')
-	socket.on('disconnect', () => console.log('user disconnected')
-  )
-})
+app.use(express.static(__dirname + '/public'))
 
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    socket.broadcast.emit('chat message', msg)
-  })
+    io.emit('online', online_list)
+
+    socket.on('online', (name) => {
+        socket.username = name
+        online_list.push(name)
+        io.emit('online', online_list)
+    })
+
+    socket.on('disconnect', () => {
+        var index = online_list.indexOf(socket.username)
+        if (index >= 0) {
+            online_list.splice(index, 1)
+        }
+        io.emit('online', online_list)
+    })
+
+    socket.on('chat', (data) => {
+        socket.broadcast.emit('chat', { name: data.name, text: data.text, time: data.time })
+    })
 })
 
 http.listen(3000, () => console.log('listening on *:3000'))
-
--- public/main.js
+++ public/main.js
@@ -1,15 +1,52 @@
-const socket = io()
+Date.prototype.today = function () { 
+    return ((this.getHours() < 10)?"0":"") 
+      + this.getHours() + ":" 
+      + ((this.getMinutes() < 10)?"0":"") 
+      + this.getMinutes()
+}
 
-$('form').submit(() => {
-	let m = $('#m').val()
-	$('#messages').append($('<li>' + '<div>' + '<p>' + m + '</p>' + '</div>' + '</li>').addClass('me'))
-    socket.emit('chat message', m)
-    $('#m').val('')
-    $('.chat-container').scrollTop($('#messages').height())
+const socket = io()
+let user_name = ''
+
+
+$('#name').submit(() => {
+    user_name = $('#name-input').val()
+    if (user_name !== '') {
+        socket.emit('online', user_name)
+        $('.modal').css('display', 'none')
+        return false
+    }
     return false
 })
 
-socket.on('chat message', (msg) => {    
-   $('#messages').append($('<li>' + '<div>' + '<p>' + msg + '</p>' + '</div>' + '</li>').addClass('received'))
-   $('.chat-container').scrollTop($('#messages').height())
+$('#text').submit(() => {
+    let text = $('#m').val()
+    let time = new Date()    
+    let local_time = time.today()
+
+    if (text !== '') {
+        $('#messages').append($('<li>' + '<div>' + '<p>' + text + '<span>' + local_time + '</span>' + '</p>' + '</div>' + '</li>').addClass('me'))
+        socket.emit('chat', { text: text, name: user_name, time: local_time })
+        $('#m').val('')
+        $('.chat-area').scrollTop($('#messages').height())
+        return false
+    }
+    return false
 })
+
+socket.on('chat', (data) => {
+    $('#messages').append($('<li>' + '<div>' + '<p>' + data.name + '</p>' + '<p>' + data.text + '<span>' + data.time + '</span>' + '</p>' + '</div>' + '</li>').addClass('received'))
+    $('.chat-area').scrollTop($('#messages').height())
+})
+
+socket.on('online', (data) => {
+    $('#online').text('')
+    let online_list = data.map((e) => {
+        return '<li>' + e + '</li>'
+    })
+    $('#online').append(online_list)
+})
+
+$('#bg-select').on('change', function() {
+  $('.chat-container').css('background-image', 'url(' + this.value + ')')
+});
